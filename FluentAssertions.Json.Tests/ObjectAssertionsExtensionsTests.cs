b3a163ea4c31dcd28fe85c0603cce62d5de5e869
Boris Modylevsky <boris.modylevsky@citi.com>
2017-02-16 00:38:23 +0200

    implement BeJsonSerializable

---
+++ FluentAssertions.Json.Tests/ObjectAssertionsExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace FluentAssertions.Json.Tests
+{
+    [TestFixture]
+    public class ObjectAssertionsExtensionsTests
+    {
+        [Test]
+        public void SerializableClass_ShouldBeJsonSerializable()
+        {
+            var obj = new SerializableClass();
+
+            obj.Should().BeJsonSerializable<SerializableClass>();
+        }
+
+        [Test]
+        public void NotSerializableClass_ShouldNotBeJsonSerializable()
+        {
+            var notJsonSerializable = new NotJsonSerializable(11);
+
+            Action action = () => notJsonSerializable.Should().BeJsonSerializable<NotJsonSerializable>();
+
+            action.ShouldThrow<AssertionException>()
+                .Where(e=>e.Message.Contains(" to be JSON serializable, but serialization failed with:"));
+        }
+
+        public class SerializableClass
+        {
+            public string Name { get; set; }
+        }
+
+        public class NotJsonSerializable
+        {
+            public NotJsonSerializable(int param)
+            {
+                
+            }
+        }
+    }
+}
